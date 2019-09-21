5e378832037b89773e4b63a8188238bdd495ff38
natsuapo <xiatianqi@csis.u-tokyo.ac.jp>
2019-09-21 15:16:14 +0900

    modify output

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ app.post('/coords', function(request, response) {
 			{
 				"fallback": "Required plain-text summary of the attachment.",
 				"color": "#36a64f",
-				"pretext": people.length + " people have joined. " + (decay_minutes - diffMins)+ " mins left before " + people[people.length -1].label + " [leader] drops out",
+				"pretext": 'lat: ' + lat + '; lon: ' + lon,
 				"title": person.label + "'s location",
 				"title_link": "https://www.google.com/maps/place/" + latlng,
 				"image_url": "https://maps.googleapis.com/maps/api/staticmap?" +
